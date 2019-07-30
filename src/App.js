98e577b5745d6afbc6a8d070e8cf3f4addbda56c
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-30 00:21:51 -0500

    Created Posts.js so that routers can be created on react.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route }  from "react-router-dom";
 import Login from './components/Login.js';
 import SignUp from "./components/SignUp";
 import Display from "./components/Display";
-import EditPosts from "./components/EditPosts.js";
-import AddPosts from "./components/AddPosts.js";
 
 function App () {
 
@@ -14,19 +12,6 @@ function App () {
   function newUser(user) {
     setUsers([...users, user])
   }
-
-
-  const [posts, setPosts] = useState([]);
-
-  const submitPost = post => setPosts([...posts, post]);
-  const update = updatedPost =>
-    setPosts([...posts.map(post => {
-        if (post.id === updatedPost.id) {
-          return updatedPost;
-        }
-        return post;
-      })
-    ]);
 
   return (
 
@@ -38,19 +23,8 @@ function App () {
       {users.map(user => (
         <Display user={user} />
       ))}
-
-      
-
       {/* <Route exact path="/" component={SignUp}/> */}
       <Login />
-
-      <div>
-        {posts.map((post, i) => (
-          <EditPosts post={post} key={i} update={update} />
-        ))}
-        <AddPosts add={submitPost} />
-      </div>
-
     </div>
 
     
---
+++ src/components/Posts.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import EditPosts from "./components/EditPosts.js";
+import AddPosts from "./components/AddPosts.js";
+
+function Posts () {
+    const [posts, setPosts] = useState([]);
+
+    const submitPost = post => setPosts([...posts, post]);
+    const update = updatedPost =>
+    setPosts([...posts.map(post => {
+        if (post.id === updatedPost.id) {
+            return updatedPost;
+        }
+        return post;
+        })
+    ]);
+    
+    return(
+    <div>
+        {posts.map((post, i) => (
+            <EditPosts post={post} key={i} update={update} />
+        ))}
+        <AddPosts add={submitPost} />
+    </div>
+    )
+}
