df3c11ee6b18cf8f8925b445bad49965537021ae
Alexis Panyathong <alexispanyathong@gmail.com>
2019-07-30 15:00:11 -0500

    SignUp updates
    I finished styling my signup form and have the basic functions working, just waiting on back end to get done.

---
+++ src/components/Posts.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import EditPosts from '/EditPosts.js';
+import AddPosts from '/AddPosts.js';
+
+function Posts() {
+    const [posts, setPosts] = useState([]);
+  
+    const submitPost = post => setPosts([...posts, post]);
+    const update = updatedPost =>
+      setPosts([...posts.map(post => {
+          if (post.id === updatedPost.id) {
+            return updatedPost;
+          }
+          return post;
+        })
+      ]);
+  
+    return (
+      <div>
+        {posts.map((post, i) => (
+          <EditPosts post={post} key={i} update={update} />
+        ))}
+        <AddPosts add={submitPost} />
+      </div>
+    );
+  }
+  
+export default Posts;
+//Mariam Farrukh
--- src/components/StyledWidgets.js
+++ src/components/StyledWidgets.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 //Alexis
 export const Sign = styled.div`
-    font-size: 12px;
     margin: 0 auto;
     margin-top: 40px;
-    border: 2px solid lightgrey;
+    border: 2px solid #E3E1E1;
+    border-radius: 12px;
+    box-shadow: 5px 5px 8px #BFBFBF;
     width: 20%;
     align-items: center;
-    height: 200px;
+    height: 400px;
     display: flex;
     justify-content: center;
 `;
@@ -17,20 +18,40 @@ export const Context = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    color: grey;
+    font-size: 16px;
+    margin-top: 25px;
+    margin-bottom: 25px;
     .form-group {
         display: flex;
         justify-content: space-between;
+        margin-bottom: 15px;
+        margin-left: 10px;
         label {
-            margin-right: 5px;
+            
+            margin-bottom: 5px;
+            display: flex;
+            justify-content: space-between;
         }
     }
 `;
 
 export const SignupBtn = styled.button`
-    color: blue;
+    background-color: #0A235C;
+    color: #FFCC00;
+    font-size: 14px;
+    border-radius: 8px;
+    padding: 8px 8px;
+    width: 40%;
+    margin: 0 auto;
+    margin-top: 5px;
+    margin-bottom: 20px;
 `;
 
+export const Header = styled.h2`
+    font-size: 18px;
+    color: #0A235C;
+    text-align: center;
+`;
 
 
 //Kelly
